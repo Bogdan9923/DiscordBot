42217ff34619ad053b5bbc7df0e1270ab75d0786
Bogdan9923 <sosdianbogdan@gmail.com>
2022-10-07 02:33:22 +0300

    Some messing around with instructions.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello')
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+
+SpecialKey = 'MTAyNzcwOTk5NDk0ODM4Mjc1MA.Gwf79N.lSjBUucTEliIxWAzr2HA8NDgH1G2VkUHUyM0YM'
+
+@client.event
+async def on_ready():
+    print('Bot logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(msg):
+    username = str(msg.author).split('#')[0]
+    user_msg = str(msg.content)
+    channel = str(msg.channel.name)
+    print(f'{username}: {user_msg} ({channel})')
+
+client.run(SpecialKey)
