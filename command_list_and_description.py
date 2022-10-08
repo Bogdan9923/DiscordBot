c3c5c73f916c35980d7542e0f17116d281ee08ad
andytm99 <andytm99@yahoo.com>
2022-10-08 21:41:28 +0300

    Added short command description and bot status

--- command_list_and_description.py
+++ command_list_and_description.py
@@ -1,5 +1,7 @@
 commandDescriptionDict = {
-    'help': 'This is the help page ',
-    'welcome': 'Welcome command description',
-    'say': 'Say command description',
+    'help': 'One command to rule them all. It is the basic command of this bot, used to inform you of capabilities'
+            ' of any other command. Including itself :).',
+    'welcome': 'This command welcomes you when you are the one who knocks at our server door.',
+    'say': 'This command transforms our bot in a little African Grey Parrot, capable of reproducing any words'
+           ' you say.',
     }
